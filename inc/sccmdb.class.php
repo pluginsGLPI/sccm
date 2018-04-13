5f6d96e43bfb41952f75e860cd5925f1c6adab39
stonebuzz <skita@teclib.com>
2018-04-13 11:00:46 +0200

    fix CS

--- inc/sccmdb.class.php
+++ inc/sccmdb.class.php
@@ -57,7 +57,7 @@ class PluginSccmSccmdb {
       );
 
       $conn = sqlsrv_connect( $host, $connectionOptions );
-      if( $conn === false ) {
+      if ($conn === false) {
              die($this->FormatErrors( sqlsrv_errors()));
       }
 
@@ -73,26 +73,26 @@ class PluginSccmSccmdb {
    function exec_query($query) {
 
       $result = sqlsrv_query($this->dbconn, $query) or die('Query error : ' . print_r(sqlsrv_errors(), true));
-      if ( $result == FALSE ){
+      if ($result == FALSE) {
          die( FormatErrors( sqlsrv_errors()));
       }
       return $result;
 
    }
 
-   function FormatErrors( $errors ){  
-     
-      foreach ( $errors as $error ){  
+   function FormatErrors( $errors ) {
+
+      foreach ($errors as $error) {
          $debug   = "";
-         $state   = "SQLSTATE: ".$error['SQLSTATE'];  
-         $code    = "Code: ".$error['code'];  
-         $message = "Message: ".$error['message'];  
+         $state   = "SQLSTATE: ".$error['SQLSTATE'];
+         $code    = "Code: ".$error['code'];
+         $message = "Message: ".$error['message'];
 
          echo $state."</br>".$code."<br>".$message."<br>";
          Toolbox::logInFile("sccm", $state.PHP_EOL.$code.PHP_EOL.$message.PHP_EOL);
       }
 
-   } 
+   }
 
 }
 
