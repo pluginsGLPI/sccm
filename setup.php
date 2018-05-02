103a749fb4d71d11bf5c0748570f7f9eeb69079d
Naylin Medina <nmedina@teclib.com>
2018-05-02 15:10:25 -0400

    docs(license): change license header
    Signed-off-by: Naylin Medina <nmedina@teclib.com>

--- setup.php
+++ setup.php
@@ -1,34 +1,27 @@
 <?php
-/*
+/**
+ * ------------------------------------------------------------------------
+ * LICENSE
  *
- -------------------------------------------------------------------------
- GLPISCCMPlugin
- Copyright (C) 2014 by teclib.
-
- http://www.teclib.com
- -------------------------------------------------------------------------
-
- LICENSE
-
- This file is part of GLPISCCMPlugin.
-
- GLPISCCMPlugin is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- GLPISCCMPlugin is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with GLPISCCMPlugin. If not, see <http://www.gnu.org/licenses/>.
- --------------------------------------------------------------------------
-*/
-
-// Original Author of file: François Legastelois <flegastelois@teclib.com>
-// ----------------------------------------------------------------------
+ * This file is part of SCCM plugin.
+ *
+ * SCCM plugin is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * SCCM plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------
+ * @author    François Legastelois <flegastelois@teclib.com>
+ * @copyright Copyright (C) 2014-2018 by Teclib' and contributors.
+ * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link      https://github.com/pluginsGLPI/sccm
+ * @link      https://pluginsglpi.github.io/sccm/
+ * ------------------------------------------------------------------------
+ */
 
 define("PLUGIN_SCCM_VERSION", "2.0.1");
 
