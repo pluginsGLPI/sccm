55fe9a36739e25fb463d2481fef6c49bcef59bfe
Cédric Anne <cedric.anne@gmail.com>
2021-02-15 16:10:52 +0100

    Add 2.3.0 version definition

--- setup.php
+++ setup.php
@@ -23,7 +23,7 @@
  * ------------------------------------------------------------------------
  */
 
-define("PLUGIN_SCCM_VERSION", "2.2.2");
+define("PLUGIN_SCCM_VERSION", "2.3.0");
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_SCCM_MIN_GLPI", "9.5");
