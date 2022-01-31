4ebb6bd56093f4af164e1160a3f26270a9b8c268
Cédric Anne <cedric.anne@gmail.com>
2022-01-31 15:59:41 +0100

    Fix licence headers

--- front/config.form.php
+++ front/config.form.php
@@ -1,26 +1,32 @@
 <?php
+
 /**
- * ------------------------------------------------------------------------
+ * -------------------------------------------------------------------------
+ * SCCM plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
  * LICENSE
  *
- * This file is part of SCCM plugin.
+ * This file is part of SCCM.
  *
- * SCCM plugin is free software; you can redistribute it and/or modify
+ * SCCM is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * SCCM plugin is distributed in the hope that it will be useful,
+ * SCCM is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * ------------------------------------------------------------------------
- * @author    François Legastelois <flegastelois@teclib.com>
- * @copyright Copyright (C) 2014-2018 by Teclib' and contributors.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SCCM. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @author    François Legastelois
+ * @copyright Copyright (C) 2014-2022 by SCCM plugin team.
  * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
  * @link      https://github.com/pluginsGLPI/sccm
- * @link      https://pluginsglpi.github.io/sccm/
- * ------------------------------------------------------------------------
+ * -------------------------------------------------------------------------
  */
 
 include ('../../../inc/includes.php');
