c4807b4fb6f3d56acf15aeb6161874175dbe844a
Cédric Anne <cedric.anne@gmail.com>
2023-06-19 13:53:01 +0200

    2.4.3

--- setup.php
+++ setup.php
@@ -29,7 +29,7 @@
  * -------------------------------------------------------------------------
  */
 
-define("PLUGIN_SCCM_VERSION", "2.4.2");
+define("PLUGIN_SCCM_VERSION", "2.4.3");
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_SCCM_MIN_GLPI", "10.0.0");
