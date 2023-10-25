0febf52ecd7f4f413385b77b84891dd4bf41d457
LAUNAY Samuel <107540223+Lainow@users.noreply.github.com>
2023-10-25 14:51:18 +0200

    Remove bad header

--- inc/inventorylog.class.php
+++ inc/inventorylog.class.php
@@ -1,34 +1,4 @@
 <?php
-
-/**
- * -------------------------------------------------------------------------
- * SCCM plugin for GLPI
- * -------------------------------------------------------------------------
- *
- * LICENSE
- *
- * This file is part of SCCM.
- *
- * SCCM is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * SCCM is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with SCCM. If not, see <http://www.gnu.org/licenses/>.
- * -------------------------------------------------------------------------
- * @author    François Legastelois
- * @copyright Copyright (C) 2014-2023 by SCCM plugin team.
- * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
- * @link      https://github.com/pluginsGLPI/sccm
- * -------------------------------------------------------------------------
- */
-
 if (!defined('GLPI_ROOT')) {
     die("Sorry. You can't access directly to this file");
 }
