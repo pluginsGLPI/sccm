586564dc0ca6f73d6936e1049a1c6b46bc5f9ee5
stonebuzz <skita@teclib.com>
2024-02-14 11:32:08 +0100

    remove useless code

--- front/inventorylog.form.php
+++ front/inventorylog.form.php
@@ -33,23 +33,19 @@ include('../../../inc/includes.php');
 
 $inventorylog = new PluginSccmInventoryLog();
 
-if (isset($_POST["validate"])) {
-    echo "validate";
-} else {
-    // if id missing
-    isset($_GET['id'])
-        ? $ID = intval($_GET['id'])
-        : $ID = 0;
+// if id missing
+isset($_GET['id'])
+    ? $ID = intval($_GET['id'])
+    : $ID = 0;
 
-    // display form
-    Html::header(
-        PluginSccmInventoryLog::getTypeName(),
-        $_SERVER["PHP_SELF"],
-        "config",
-        PluginSccmMenu::class,
-        "sccm_inventorylog"
-    );
+// display form
+Html::header(
+    PluginSccmInventoryLog::getTypeName(),
+    $_SERVER["PHP_SELF"],
+    "config",
+    PluginSccmMenu::class,
+    "sccm_inventorylog"
+);
 
-    $inventorylog->display(['id' => $ID]);
-    Html::footer();
-}
+$inventorylog->display(['id' => $ID]);
+Html::footer();
