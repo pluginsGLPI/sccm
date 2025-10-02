621f7be48c53120263bfa2f89db2a5b8705b4933
MyuTsu <gaetan.langlois31@gmail.com>
2025-10-02 11:38:01 +0200

    lint

--- inc/sccmdb.class.php
+++ inc/sccmdb.class.php
@@ -56,7 +56,7 @@ class PluginSccmSccmdb
             "Uid" => $user,
             "PWD" => $password,
             "CharacterSet" => "UTF-8",
-            "TrustServerCertificate" => $verify_ssl_cert
+            "TrustServerCertificate" => $verify_ssl_cert,
         ];
 
         $this->dbconn = sqlsrv_connect($host, $connectionOptions);
