ba0ce6b44887777fb5391211881302bc5cd59bf5
Romain B. <8530352+Rom1-B@users.noreply.github.com>
2026-08-04 15:20:26 +0200

    2.6.1 (#188)
    * 2.6.1
    
    * fix URL
    
    * Update setup.php
    
    * Update sccm.xml
    
    ---------
    
    Co-authored-by: Langlois Gaëtan <64356364+MyvTsv@users.noreply.github.com>

--- setup.php
+++ setup.php
@@ -63,9 +63,9 @@ declare(strict_types=1);
 use Glpi\Plugin\Hooks;
 
 /** @phpstan-ignore theCodingMachineSafe.function (safe to assume this isn't already defined) */
-define('PLUGIN_SCCM_VERSION', '2.6.0');
+define('PLUGIN_SCCM_VERSION', '2.6.1');
 /** @phpstan-ignore theCodingMachineSafe.function (safe to assume this isn't already defined) */
-define("PLUGIN_SCCM_MIN_GLPI", "11.0.00");
+define("PLUGIN_SCCM_MIN_GLPI", "11.0.0");
 /** @phpstan-ignore theCodingMachineSafe.function (safe to assume this isn't already defined) */
 define("PLUGIN_SCCM_MAX_GLPI", "11.0.99");
 
